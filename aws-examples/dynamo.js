5597d2b8f452012772dcae6f4447004b4de4f3b1
Steven Guerrini <sguerrini@gmail.com>
2015-01-26 07:03:10 -0800

    MMT Issue 279 Complete - event driven sequence

--- aws-examples/dynamo.js
+++ aws-examples/dynamo.js
@@ -28,22 +28,40 @@ console.log("Starting DynamoDB interactions...");
 getItem(1);
 
 
-console.log("===== Writing item =====");
+// console.log("===== Writing item =====");
+
+// var params = {
+// 	Item: {
+// 		"Obj Id": { "N": "4" },
+// 		"Foo": { "S": "Bar" },
+// 		"Status": { "S": "Written!" }	
+// 	},
+// 	TableName: 'guerrini-first-or'
+// };
+
+
+// db.putItem(params, function(err, data) {
+// 	if (err) console.log(err, err.stack); // an error occurred
+// 	else     console.log(data);           // successful response
+// });	
+
+// console.log("===== Reading New Item =====");
+// getItem(2);
+
+
+
+console.log("===== Updating Item =====");
 
 var params = {
-	Item: {
-		"Obj Id": { "N": "2" },
-		"Foo": { "S": "Bar" },
-		"Status": { "S": "Written!" }	
+	Key: { "Obj Id": { "N": "2" } },
+	AttributeUpdates: {	
+		"Status": { Action: "PUT", Value: {S: "X"} }
 	},
 	TableName: 'guerrini-first-or'
 };
 
 
-db.putItem(params, function(err, data) {
+db.updateItem(params, function(err, data) {
 	if (err) console.log(err, err.stack); // an error occurred
 	else     console.log(data);           // successful response
 });	
-
-console.log("===== Reading New Item =====");
-getItem(2);
